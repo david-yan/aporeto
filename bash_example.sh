72c5daf9b148290d6ae98ee8dbafbbb05b2aad37
David Yan <david.yan@berkeley.edu>
2016-04-12 11:50:07 -0700

    finished p1

--- bash_example.sh
+++ bash_example.sh
@@ -1,10 +1,18 @@
 STATES=(Alabama Alaska Arizona Arkansas California Colorado Connecticut Delaware Florida Georgia Hawaii Idaho Illinois Indiana Iowa Kansas Kentucky Louisiana Maine Maryland Massachusetts Michigan Minnesota Mississippi Missouri Montana Nebraska Nevada "New Hampshire" "New Jersey" "New Mexico" "New York" "North Carolina" "North Dakota" Ohio Oklahoma Oregon Pennsylvania "Rhode Island" "South Carolina" "South Dakota" Tennessee Texas Utah Vermont Virginia Washington "West Virginia" Wisconsin Wyoming)
 
+function printHelp() {
+	echo "Usage:"
+	echo " bash_example --create-file=<filename> [options]"
+	echo "	options:"
+	echo "	  --no-prompt		Automatically overwrites file"
+	echo "	  --verbose"
+	echo " bash_example [--help|-h]"
+}
 
 #Check for no arguments
 if [ $# -eq 0 ]; then
 	echo "ERROR: Missing arguments or options"
-	echo "(run $0 --help|-h for help)"
+	printHelp
 	exit 1
 fi
 
@@ -12,7 +20,7 @@ if [[ $1 == --create-file=* ]]; then
 	filename=$(echo $1| cut -d"=" -f 2)
 	if [ -z $filename ]; then
 		echo "ERROR: cannot have empty filename"
-		echo "(run $0 --help|-h for help)"
+		printHelp
 		exit 1
 	fi
 
@@ -24,27 +32,27 @@ if [[ $1 == --create-file=* ]]; then
 		if [ $option = "--no-prompt" ]; then
 			if $NOPROMPT; then
 				echo "ERROR: Repeated option --no-prompt"
-				echo "(run $0 --help|-h for help)"			
+				printHelp			
 				exit 1
 			fi
 			NOPROMPT=true
 		elif  [ $option = "--verbose" ]; then
 			if $VERBOSE; then
 				echo "ERROR: Repeated option --verbose"
-				echo "(run $0 --help|-h for help)"
+				printHelp
 				exit 1
 			fi
 			VERBOSE=true
 		elif [[ $option = --create-file=* ]]; then
 			if $CREATE_FILE; then
 				echo "ERROR: Cannot create multiple files"
-				echo "(run $0 --help|-h for help)"
+				printHelp
 				exit 1
 			fi
 			CREATE_FILE=true
 		else
 			echo "ERROR: Invalid argument $option"
-			echo "(run $0 --help|-h for help)"
+			printHelp
 			exit 1
 		fi
 	done
@@ -76,16 +84,14 @@ if [[ $1 == --create-file=* ]]; then
 elif [ $1 = "-h" ] || [ $1 = "--help" ]; then
 	if [ ! $# -eq 1 ]; then
 		echo "ERROR: Invalid arguments"
-		echo "(run $0 --help|-h for help)"
+		printHelp
 		exit 1
 	fi
-	echo "Usage: bash_example --create-file=<filename> [options]"
-	echo "	options:"
-	echo "	  --no-prompt		Automatically overwrites file"
-	echo "	  --verbose"
-else
+	printHelp
+	else
 	echo "ERROR: Invalid arguments"
-	echo "(run $0 --help|-h for help)"
+	printHelp
 	exit 1
 fi
 exit 0
+
